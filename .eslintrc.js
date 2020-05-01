6c1a034651dff1da1ba6d74b6aa735dd5acc5dbe
Yohuan <b00502053@ntu.edu.tw>
2020-05-02 01:11:36 +0800

    set eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,28 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    'eslint-plugin-jest'
+    'eslint-plugin-import',
+    'eslint-plugin-jest',
   ],
   rules: {
-    'max-len': ['warn', { 'code': 120 } ],
-    'no-underscore-dangle': ["off"],
-    'import/prefer-default-export': ["off"],
+    'max-len': ['warn', { code: 120 } ],
+    'no-underscore-dangle': ['off'],
+    'import/no-cycle': ['off'],
+    'import/no-unresolved': ['error', { commonjs: true } ],
+    'import/order': ['warn', {
+      'groups': [
+        'builtin',
+        'external',
+        [
+          'index',
+          'internal',
+          'parent',
+          'sibling',
+          'unknown',
+        ],
+      ],
+      'newlines-between': 'always',
+    }],
+    'import/prefer-default-export': ['off'],
   },
 };
